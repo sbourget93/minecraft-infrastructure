52900488f353674fd1c3128ee63e9376f7f80ff6
Stephen Bourget <sbourget93@gmail.com>
2019-03-30 11:38:58 -0700

    clone in correct dir

--- terraform/modules/minecraft_server/launch_configuration.tf
+++ terraform/modules/minecraft_server/launch_configuration.tf
@@ -16,9 +16,9 @@ resource "aws_launch_configuration" "minecraft" {
     sudo yum update -y
     sudo yum install git -y
 
-    git clone --single-branch --branch "${var.branch_name}" https://github.com/sbourget93/minecraft.git
+    git clone --single-branch --branch "${var.branch_name}" https://github.com/sbourget93/minecraft-server.git
 
-    cd minecraft/
+    cd minecraft-server/
     chmod +x ./bootstrap.sh
     ./bootstrap.sh
   EOF
